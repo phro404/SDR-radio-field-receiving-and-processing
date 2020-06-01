72a4a9d10a7e78eed4a5180be128c16e6ffcba1a
cade404 <64841516+cade404@users.noreply.github.com>
2020-06-01 19:40:59 +0200

    Socket Server modified
    Socket Server is now modified and ready for use.

---
+++ src/Socket-Server_modified.py
@@ -0,0 +1,71 @@
+import socket	
+import json
+import time
+import random
+ 
+while True:
+	#create a socket object 
+	s = socket.socket()		  
+	print ("Socket successfully created")
+		  
+	s.bind(('localhost', 8012)) #TODO: choose correct arguments here
+	print ("socket binded to 8012") 
+	  
+	s.listen(5)	  
+	print ("socket is listening")			
+	   
+	c, addr = s.accept()	  
+	print ('Got connection from', addr) 
+
+
+	#create test data with json
+
+	#dictionary for Mode AC
+	telegram_mode_ac = {
+		'type': 'mode_ac',
+		'payload': '7311',
+		'amplitude': 1.0,
+		'shift': 0.0,
+		'phase': 0	}
+
+	#dictionary for Mode S Short
+	telegram_mode_s_short = {
+		'type': 'mode_s_short',
+		'format_number': 'DF00',
+		'payload': None,
+		'amplitude': 1.0,
+		'shift': 0.0,
+		'phase': 0	}
+		
+	#dictionary for Mode S Long
+	telegram_mode_s_long = {
+		'type': 'mode_s_long',
+		'format_number': 'DF16',
+		'payload': None,
+		'amplitude': 1.0,
+		'shift': 0.0,
+		'phase': 0	}
+
+	#sending test data
+	try:
+		while True: 
+			time.sleep(3)
+			randomint = random.randint(1,30) 	#random value between 1 and 30 for the rate
+			
+			#dictionary for the testtelegrams
+			testtelegrams = {
+				'level': -50,
+				'rate': randomint,
+				'amount': 30,
+				'samplerate': 20000000,
+				'telegrams': [telegram_mode_ac,telegram_mode_s_short,telegram_mode_s_long]	}
+				
+			testtelegrams = json.dumps(testtelegrams) #converting dictionary in json-string
+			c.send(testtelegrams.encode('ascii')) 	
+			print("Data sent!")
+	except:
+		pass
+		
+	finally:
+		c.close() # Close the connection with the client
+			
