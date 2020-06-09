51cf260469964c819970f5fb0089b5368cd6ab90
P <64910106+TaskFailedSuccessfully@users.noreply.github.com>
2020-06-09 18:00:40 +0200

    Now writing two new files for liveplots
    two new files with consistent names are beeing written and deleted after exiting

--- src/fileWriter.py
+++ src/fileWriter.py
@@ -3,7 +3,7 @@ import visualization
 from datetime import datetime	
 from time import sleep, time
 
-class FileWriter(object):							
+class FileWriter(object):
 
 	def __init__(self): 
 		self.lvl = ""											#defines string for lvl_reply_date prints
@@ -16,31 +16,32 @@ class FileWriter(object):
 		self.lvlFirstLine = "time;rx_cnt;rx_avg_lvl;curr_ch_occ;curr_planes;test_tx_cnt;test_rx_succ_cnt_s;test_rx_succ_cnt_l;test_rx_succ_cnt_ac;test_succ_lvl_s;test_succ_lvl_l;test_succ_lvl_ac;test_avg_lvl_s;test_avg_lvl_l;test_avg_lvl_ac\n"		#sets the first line for every lvl-file
 		
 		self.ampFirstLine = "time,type,total,-90,-89,-88,-87,-86,-85,-84,-83,-82,-81,-80,-79,-78,-77,-76,-75,-74,-73,-72,-71,-70,-69,-68,-67,-66,-65,-64,-63,-62,-61,-60,-59,-58,-57,-56,-55,-54,-53,-52,-51,-50,-49,-48,-47,-46\n"				#sets the first line for every amp-file
-		
+	
 	def write(self):	
+		############################################---.csv-write-start---###############################################
 		now = datetime.now()
 		if (self.d !=  now.strftime("%Y.%m.%d_%H")):							#check for new hour
 			self.d = now.strftime("%Y.%m.%d_%H")							#set d as timedefinition
-			self.name_lvl = "../data/lvl_reply_" + self.d + ".csv"				#set name lvl_reply_date
-			self.name_amp = "../data/amp_hist_" + self.d + ".csv"				#set name amp_hist_date
+			self.name_lvl = "../data/lvl_reply_" + self.d + ".csv"					#set name lvl_reply_date
+			self.name_amp = "../data/amp_hist_" + self.d + ".csv"					#set name amp_hist_date
 			
 			f = open(self.name_lvl ,"a")								#generating for empty-test lvl, attend to save possible content
 			f.close()
 			f = open(self.name_amp ,"a")								#generating for empty-test amp, attend to save possible content
 			f.close()
 			
-			if os.stat(self.name_lvl).st_size == 0:						#if lvl is empty write firstline
+			if os.stat(self.name_lvl).st_size == 0:							#if lvl is empty write firstline
 				f = open(self.name_lvl ,"w+")							#open data with name, if not existing create
 				f.write(self.lvlFirstLine)							#print first line in file
 				f.close()
 			
-			if os.stat(self.name_amp).st_size == 0:						#if amp is empty write firstline
+			if os.stat(self.name_amp).st_size == 0:							#if amp is empty write firstline
 				f = open(self.name_amp ,"w+")							#open data with name, if not existing create
 				f.write(self.ampFirstLine)							#print first line in file
 				f.close()
 			
-			self.orderedList = [os.path.abspath(self.name_lvl), os.path.abspath(self.name_amp)]
-		
+		self.name_lvl = "../data/lvl_reply_" + self.d + ".csv"						#for when same hour, write in the right file!
+		self.name_amp = "../data/amp_hist_" + self.d + ".csv"
 		
 		if (len(self.lvl) != 0):
 			f = open(self.name_lvl ,"a")								#open data with name, if not existing create, attand to written text
@@ -51,9 +52,44 @@ class FileWriter(object):
 			f = open(self.name_amp ,"a")								#open data with name, if not existing create, attand to written text
 			f.write(self.amp)									#print string in data
 			f.close()
+		
+		############################################---.csv-write-end---#####################################################
+		
+		############################################---Liveplot-write-start---###############################################
+		
+		self.name_lvl = "../data/liveplot_lvl.csv"							#new name for printing into the level-liveplot file
+		self.name_amp = "../data/liveplot_amp_hist.csv"							#new name for printing into the amp-hist_liveplot file
+		
+		
+		f = open(self.name_lvl ,"a")									#generating for empty-test lvl, attend to save possible content
+		f.close()
+		f = open(self.name_amp ,"a")									#generating for empty-test amp, attend to save possible content
+		f.close()
+		
+		if os.stat(self.name_lvl).st_size == 0:								#if lvl is empty write firstline
+			f = open(self.name_lvl ,"w+")								#open data with name, if not existing create
+			f.write(self.lvlFirstLine)								#print first line in file
+			f.close()
 			
+		if os.stat(self.name_amp).st_size == 0:								#if amp is empty write firstline
+			f = open(self.name_amp ,"w+")								#open data with name, if not existing create
+			f.write(self.ampFirstLine)								#print first line in file
+			f.close()	
+		
+		if (len(self.lvl) != 0):
+			f = open(self.name_lvl ,"a")								#open data with name, if not existing create, attand to written text
+			f.write(self.lvl)									#print string in data
+			f.close()
+		
+		if (len(self.lvl) != 0):
+			f = open(self.name_amp ,"a")								#open data with name, if not existing create, attand to written text
+			f.write(self.amp)									#print string in data
+			f.close()
+		
+		self.orderedList = [os.path.abspath(self.name_lvl), os.path.abspath(self.name_amp)]	
 		visualization.visualization(self.orderedList, True)
-		self.orderedList = [os.path.abspath(self.name_lvl), os.path.abspath(self.name_amp)]
+		
+		############################################---Liveplot-write-end---###############################################
 	
 	def sort(self, raw_pipe_out):
 		local_buffer = []										#declines the local buffer as an empty array
@@ -62,7 +98,7 @@ class FileWriter(object):
 		AClist = {}											#declines AClist as an empty list
 		Dlist = {}											#declines Dlist as an empty list
 		
-		while(not raw_pipe_out.poll()):								#pollingprocess
+		while(not raw_pipe_out.poll()):									#pollingprocess
 			sleep(0.2)										#waittime for polling
 		
 		while(raw_pipe_out.poll()):									#polling
@@ -97,8 +133,8 @@ class FileWriter(object):
 			self.amp = self.amp + Llist["time"]+"," + Llist["type"]+"," + Llist["total"]+"," + Llist["-90"]+"," + Llist["-89"]+"," + Llist["-88"] + "," + Llist["-87"] + "," + Llist["-86"] + "," + Llist["-85"] + "," + Llist["-84"] + "," + Llist["-83"] + "," + Llist["-82"] + "," + Llist["-81"] + "," + Llist["-80"] + "," + Llist["-79"] + "," + Llist["-78"] + "," + Llist["-77"] + "," + Llist["-76"] + "," + Llist["-75"] + "," + Llist["-74"] + "," + Llist["-73"] + "," + Llist["-72"] + "," + Llist["-71"] + "," + Llist["-70"] + "," + Llist["-69"] + "," + Llist["-68"] + "," + Llist["-67"] + "," + Llist["-66"] + "," + Llist["-65"] + "," + Llist["-64"] + "," + Llist["-63"] + "," + Llist["-62"] + "," + Llist["-61"] + "," + Llist["-60"] + "," + Llist["-59"] + "," + Llist["-58"] + "," + Llist["-57"] + "," + Llist["-56"] + "," + Llist["-55"] + "," + Llist["-54"] + "," + Llist["-53"] + "," + Llist["-52"] + "," + Llist["-51"] + "," + Llist["-50"] + "," + Llist["-49"] + "," + Llist["-48"] + "," + Llist["-47"] + "," + Llist["-46"] + "\n"
 		if not (len(AClist) < 48):
 			self.amp = self.amp + AClist["time"]+"," + AClist["type"]+"," + AClist["total"]+"," + AClist["-90"]+"," + AClist["-89"]+"," + AClist["-88"]+"," + AClist["-87"]+"," + AClist["-86"]+"," + AClist["-85"]+"," + AClist["-84"]+"," + AClist["-83"]+"," + AClist["-82"]+"," + AClist["-81"]+"," + AClist["-80"]+"," + AClist["-79"]+"," + AClist["-78"]+"," + AClist["-77"]+"," + AClist["-76"]+"," + AClist["-75"]+"," + AClist["-74"]+"," + AClist["-73"]+"," + AClist["-72"]+"," + AClist["-71"]+"," + AClist["-70"]+"," + AClist["-69"]+"," + AClist["-68"]+"," + AClist["-67"]+"," + AClist["-66"]+"," + AClist["-65"]+"," + AClist["-64"]+"," + AClist["-63"]+"," + AClist["-62"]+"," + AClist["-61"]+"," + AClist["-60"]+"," + AClist["-59"]+"," + AClist["-58"]+"," + AClist["-57"]+"," + AClist["-56"]+"," + AClist["-55"]+"," + AClist["-54"]+"," + AClist["-53"]+"," + AClist["-52"]+"," + AClist["-51"]+"," + AClist["-50"]+"," + AClist["-49"]+"," + AClist["-48"]+"," + AClist["-47"]+"," + AClist["-46"] + "\n"
-
 		
+	
 	def run(self, in_pipe, exit):
 		print("FileWriter: Entered RUN()")
 		while (not exit.is_set()):
@@ -106,4 +142,5 @@ class FileWriter(object):
 			self.sort(in_pipe)
 			#print(2)
 			self.write()
-			
+		os.remove("../data/liveplot_lvl.csv")
+		os.remove("../data/liveplot_amp_hist.csv")
