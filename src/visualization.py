981ba5cfcb1e23ce258408d5245f694ceea1f6cd
inhe404 <64841431+inhe404@users.noreply.github.com>
2020-06-13 12:38:55 +0200

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -9,7 +9,7 @@ import datetime
 #=================================== Import of the relevant libraries ======================================
 
 def visualization(orderedList, livePlot):
-	mlp.use('TkAgg') 
+	mlp.use('Agg') 
 	
 	#=================================== Implementation of all Variables =======================================
 	row_counter_data_paths = 0	  # Counter for the index number of orderedList
