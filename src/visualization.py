6894300ba630fedab458d0106c7d6815763d5ed0
inhe404 <64841431+inhe404@users.noreply.github.com>
2020-06-13 12:46:49 +0200

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -2,14 +2,17 @@
 import csv
 import time
 import matplotlib as mlp
-#mlp.use('TkAgg') 
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 #=================================== Import of the relevant libraries ======================================
 
 def visualization(orderedList, livePlot):
-	mlp.use('Agg') 
+	
+	if livePlot == True:
+		mlp.use('Agg') 
+	else:
+		mlp.use('TkAgg')
 	
 	#=================================== Implementation of all Variables =======================================
 	row_counter_data_paths = 0	  # Counter for the index number of orderedList
