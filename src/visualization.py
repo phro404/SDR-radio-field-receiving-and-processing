1a6d0d6cebb322ae211fd9e46c4aca459f3fe95f
inhe404 <64841431+inhe404@users.noreply.github.com>
2020-06-14 16:57:45 +0200

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -204,10 +204,13 @@ def visualization(orderedList, livePlot):
 	
 	# Get the star time and the stop time as a timestamp:
 	time_begin = time.mktime((int(str_time_begin[0:4]), int(str_time_begin[5:7]), int(str_time_begin[8:10]), int(str_time_begin[11:13]), int(str_time_begin[14:16]), int(str_time_begin[17:19]), 0, 0, 0)) - float(pro_val)
-	#str_time_begin = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time_begin))
+	
 	print(str_time_begin)
 	str_time_begin = datetime.fromtimestamp(time_begin).strftime('%Y-%m-%d %H:%M:%S')
 	print(str_time_begin)
+	str_time_begin = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time_begin))
+	print(str_time_begin)
+	
 	time_end = time.mktime((int(str_time_end[0:4]), int(str_time_begin[5:7]), int(str_time_end[8:10]), int(str_time_end[11:13]), int(str_time_end[14:16]), int(str_time_end[17:19]), 0, 0, 0))
 	
 	# Get the time space:
