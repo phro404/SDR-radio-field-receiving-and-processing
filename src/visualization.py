f6e72a8780e2517afac4a139aa1511795c811634
inhe404 <64841431+inhe404@users.noreply.github.com>
2020-06-14 13:10:25 +0200

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -5,12 +5,13 @@ import matplotlib as mlp
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime
-
 import os
 import subprocess, sys
 #=================================== Import of the relevant libraries ======================================
 
 def visualization(orderedList, livePlot):
+	
+	print('visualization started')
 	
 	if livePlot == True:
 		mlp.use('Agg') 
@@ -18,84 +19,83 @@ def visualization(orderedList, livePlot):
 		mlp.use('TkAgg')
 	
 	#=================================== Implementation of all Variables =======================================
-	row_counter_data_paths = 0	  # Counter for the index number of orderedList
+	row_counter_data_paths = 0		# Counter for the index number of orderedList
 		
-	row_counter_lvl_reply = 0	   # Counter for all rows in the level_reply CSV file
-	data_row_counter_lvl_reply = 0  # Counter of all rows with useful data in the level_reply CSV file
-	ac_test_rx_succ_sum = 0		 # Sum of all A/C test replies, which have been received successfully
-	s_long_test_rx_succ_sum = 0	 # Sum of all Mode S Long test replies, which have been received successfully
-	s_short_test_rx_succ_sum = 0	# Sum of all Mode S Short test replies, which have been received successfully
-	test_tx_sum = 0				 # Sum of all test replies, which have been transmitted
-	step_level = 1				  # Step width for the list of levels in the successrate chart
-	curr_planes = 0				 # Average number of detected planes
-	occupancy_channel_sum = 0	   # Occupancy of the channel in s
+	row_counter_lvl_reply = 0		# Counter for all rows in the level_reply CSV file
+	data_row_counter_lvl_reply = 0		# Counter of all rows with useful data in the level_reply CSV file
+	ac_test_rx_succ_sum = 0			# Sum of all A/C test replies, which have been received successfully
+	s_long_test_rx_succ_sum = 0		# Sum of all Mode S Long test replies, which have been received successfully
+	s_short_test_rx_succ_sum = 0		# Sum of all Mode S Short test replies, which have been received successfully
+	test_tx_sum = 0				# Sum of all test replies, which have been transmitted
+	step_level = 1				# Step width for the list of levels in the successrate chart (max = 1 !!!)
+	curr_planes = 0				# Average number of detected planes
+	occupancy_channel = 0			# Occupancy of the channel in s
 
-	level_for_successrate_chart = np.arange(-90, -45, step_level)		   # List of levels for the successrate chart
-	counter_ac_test_replies_per_level = np.zeros(int(45/step_level))		# List with the number of all A/C test replies per level
+	level_for_successrate_chart = np.arange(-90, -45, step_level)		# List of levels for the successrate chart
+	counter_ac_test_replies_per_level = np.zeros(int(45/step_level))	# List with the number of all A/C test replies per level
 	counter_s_long_test_replies_per_level = np.zeros(int(45/step_level))	# List with the number of all Mode S Long test replies per level
 	counter_s_short_test_replies_per_level = np.zeros(int(45/step_level))   # List with the number of all Mode S Short test replies per level
-	ac_test_reply_succ = np.zeros(int(45/step_level))					   # List of A/C successrate per level
-	s_long_test_reply_succ = np.zeros(int(45/step_level))				   # List of Mode S Long successrate per level
-	s_short_test_reply_succ = np.zeros(int(45/step_level))				  # List of Mode S Short successrate per level
+	ac_test_reply_succ = np.zeros(int(45/step_level))			# List of A/C successrate per level
+	s_long_test_reply_succ = np.zeros(int(45/step_level))			# List of Mode S Long successrate per level
+	s_short_test_reply_succ = np.zeros(int(45/step_level))			# List of Mode S Short successrate per level
 
 
-	row_counter_amp_hist = 0		# Counter for all rows in the amp_hist CSV file
-	str_time_begin = "empty"		# Start time
-	str_time_end = "empty"		  # End time
-	time_begin = 0				  # Start time as timestamp
-	time_end = 0					# End time as timestamp
-	time_space = 0				  # Time space
-	all_replies_sum = 0			 # Sum of all received replies
-	ac_replies_sum = 0			  # Sum of all A/C replies
-	s_long_replies_sum = 0		  # Sum of all Mode S Long replies
-	s_short_replies_sum = 0		 # Sum of all Mode S Short replies
+	row_counter_amp_hist = 0	# Counter for all rows in the amp_hist CSV file
+	str_time_begin = "empty"	# Start time
+	str_time_end = "empty"		# End time
+	time_begin = 0			# Start time as timestamp
+	time_end = 0			# End time as timestamp
+	time_space = 0			# Time space
+	all_replies_sum = 0		# Sum of all received replies
+	ac_replies_sum = 0		# Sum of all A/C replies
+	s_long_replies_sum = 0		# Sum of all Mode S Long replies
+	s_short_replies_sum = 0		# Sum of all Mode S Short replies
 	   
-	list_level_AC = np.zeros(45)							# List for the current number of A/C replies
-	list_level_S_long = np.zeros(45)						# List for the current number of Mode S Long replies
-	list_level_S_short = np.zeros(45)					   # List for the current number of Mode S Short replies
-	level_for_distribution_chart = np.arange(-90, -45, 1)   # List of levels for flight replies
+	list_level_AC = np.zeros(45)				# List for the current number of A/C replies
+	list_level_S_long = np.zeros(45)			# List for the current number of Mode S Long replies
+	list_level_S_short = np.zeros(45)			# List for the current number of Mode S Short replies
+	level_for_distribution_chart = np.arange(-90, -45, 1)	# List of levels for flight replies
 	#=================================== Implementation of all Variables =======================================
 
-	print('visualization started')
 	#================================ Collecting the data from the CSV files ===================================
-	for line in orderedList:  # Read Path List
+	for line in orderedList:	# Read Path List
    
 		if (row_counter_data_paths % 2) == 0:
 	 
-			# Read lvl_reply CSV file
+			# Read lvl_reply CSV file:
 			with open(orderedList[row_counter_data_paths]) as csv_data_lvl_reply:
 				lvl_reply_data = csv.reader(csv_data_lvl_reply, delimiter=',')
 			
-				row_counter_lvl_reply = 0   # Reset the row counter for the level_reply CSV
+				row_counter_lvl_reply = 0	# Reset the row counter for the level_reply CSV
 
-				# Fill the lists and variables with data of the level_reply CSV
+				# Fill the lists and variables with data of the level_reply CSV:
 				for row in lvl_reply_data:
 		
-					if row_counter_lvl_reply > 0:	 # We dont't need the fisrt row, because it only includes the topics.  
+					if row_counter_lvl_reply > 0:	# We dont't need the fisrt row, because it only includes the topics.  
 			
 						for column in range(int(45/step_level)):
 							if (float(row[14]) >= (level_for_successrate_chart[column] - (step_level/2))) & (float(row[14]) < (level_for_successrate_chart[column] + (step_level/2))):					
 								if row[11] != '':
-									counter_ac_test_replies_per_level[column] += 1							  # Count all received A/C test replies in this range
-									ac_test_reply_succ[column] = ac_test_reply_succ[column] + float(row[11])	 # Build the sum of all A/C successrates in this range
+									counter_ac_test_replies_per_level[column] += 1					# Count all received A/C test replies in this range
+									ac_test_reply_succ[column] = ac_test_reply_succ[column] + float(row[11])	# Build the sum of all A/C successrates in this range
 					
 							if (float(row[13]) >= (level_for_successrate_chart[column] - (step_level/2))) & (float(row[13]) < (level_for_successrate_chart[column] + (step_level/2))):
 								if row[10] != '':
-									counter_s_long_test_replies_per_level[column] += 1								  # Count all received Mode S Long test replies in this range
-									s_long_test_reply_succ[column] = s_long_test_reply_succ[column] + float(row[10])	 # Build the sum of all Mode S Long successrates in this range
+									counter_s_long_test_replies_per_level[column] += 1					# Count all received Mode S Long test replies in this range
+									s_long_test_reply_succ[column] = s_long_test_reply_succ[column] + float(row[10])	# Build the sum of all Mode S Long successrates in this range
 
 							if (float(row[12]) >= (level_for_successrate_chart[column] - (step_level/2))) & (float(row[12]) < (level_for_successrate_chart[column] + (step_level/2))):
 								if row[9] != '':
-									counter_s_short_test_replies_per_level[column] += 1								  # Count all received Mode S Short test replies in this range
+									counter_s_short_test_replies_per_level[column] += 1					# Count all received Mode S Short test replies in this range
 									s_short_test_reply_succ[column] = s_short_test_reply_succ[column] + float(row[9])	# Build the sum of all Mode S Short successrates in this range
 
-						ac_test_rx_succ_sum = ac_test_rx_succ_sum + int(row[8])			 # Build the sum of all A/C test replies, which have been received successfully
-						s_long_test_rx_succ_sum = s_long_test_rx_succ_sum + int(row[7])	 # Build the sum of all Mode S Long test replies, which have been received successfully
-						s_short_test_rx_succ_sum = s_short_test_rx_succ_sum + int(row[6])   # Build the sum of all Mode S Short test replies, which have been received successfully
+						ac_test_rx_succ_sum = ac_test_rx_succ_sum + int(row[8])			# Build the sum of all A/C test replies, which have been received successfully
+						s_long_test_rx_succ_sum = s_long_test_rx_succ_sum + int(row[7])	 	# Build the sum of all Mode S Long test replies, which have been received successfully
+						s_short_test_rx_succ_sum = s_short_test_rx_succ_sum + int(row[6])	# Build the sum of all Mode S Short test replies, which have been received successfully
 
-						test_tx_sum = test_tx_sum + int(row[5])							 # Build the sum of all test replies, which have been transmitted
-						curr_planes = curr_planes + int(row[4])							 # Build the sum of all numbers for detected planes
-						occupancy_channel_sum = occupancy_channel_sum + float(row[3])	   # Build the sum of the hole occupancy of the channel
+						test_tx_sum = test_tx_sum + int(row[5])					# Build the sum of all test replies, which have been transmitted
+						curr_planes = curr_planes + int(row[4])					# Build the sum of all numbers for detected planes
+						occupancy_channel = occupancy_channel + float(row[3])	   		# Build the sum of the hole occupancy of the channel
 
 					row_counter_lvl_reply += 1  # Increment the level_reply row counter
 	
@@ -105,58 +105,58 @@ def visualization(orderedList, livePlot):
 				
 			# Read amp_hist CSV file
 			with open(orderedList[row_counter_data_paths]) as csvdata_amp_hist:
-				amp_hist_data = csv.reader(csvdata_amp_hist, delimiter=',') # we need ; for reading amp_hist_2019-11-07_test
+				amp_hist_data = csv.reader(csvdata_amp_hist, delimiter=',')
 
 				row_counter_amp_hist = 0	# Reset the row counter for the amp_hist CSV
 
-				# Fill the lists and variables with data of the amp_hist CSV file
+				# Fill the lists and variables with data of the amp_hist CSV file:
 				for row in amp_hist_data:
 		   
 					if row_counter_amp_hist > 0:	# We dont't need the fisrt row, because it only includes the topics. 
 			
-						# Build the sum of all A/C replies per level
+						# Build the sum of all A/C replies per level:
 						if row[1] == "A/C Reply":
 							for column in range(45):			
 								if row[column + 3] != '':
 									list_level_AC[column] = list_level_AC[column] + int(row[column + 3])	
 			
-						# Build the sum of all Mode S Long replies per level
+						# Build the sum of all Mode S Long replies per level:
 						if row[1] == "S Long Reply":
 							for column in range(45):			
 								if row[column + 3] != '':
 									list_level_S_long[column] = list_level_S_long[column] + int(row[column + 3])
 			
-						# Build the sum of all Mode S Short replies per level
+						# Build the sum of all Mode S Short replies per level:
 						if row[1] == "S Short Reply":
 							for column in range(45):			
 								if row[column + 3] != '':
 									list_level_S_short[column] = list_level_S_short[column] + int(row[column + 3])
 
-						# Build the sum of all received replies
+						# Build the sum of all received replies:
 						all_replies_sum = all_replies_sum + int(row[2])
 
-						# Build the sum of all A/C replies
+						# Build the sum of all A/C replies:
 						if ((row[1] == "A/C Reply") & (row[2] != '')):
 							ac_replies_sum = ac_replies_sum + int(row[2])
 
-						# Build the sum of all Mode S Long replies
+						# Build the sum of all Mode S Long replies:
 						if ((row[1] == "S Long Reply") & (row[2] != '')):
 							s_long_replies_sum = s_long_replies_sum + int(row[2])
 
-						# Build the sum of all Mode S Short replies
+						# Build the sum of all Mode S Short replies:
 						if ((row[1] == "S Short Reply") & (row[2] != '')):
 							s_short_replies_sum = s_short_replies_sum + int(row[2])
 
-						# Remember the start time
+						# Remember the start time:
 						if (row_counter_data_paths == 1) & (row_counter_amp_hist == 1):
 							str_time_begin = row[0]
 			
-						# Remember the last timestamp
+						# Remember the last timestamp:
 						str_time_end = row[0]
 			
-					row_counter_amp_hist += 1   # Increment the amp_hist row counter
+					row_counter_amp_hist += 1	# Increment the amp_hist row counter
 	
-		row_counter_data_paths += 1  # Increment the data_paths row counter
+		row_counter_data_paths += 1	# Increment the data_paths row counter
 
 		
 	if data_row_counter_lvl_reply == 0:
@@ -270,7 +270,7 @@ def visualization(orderedList, livePlot):
 	plt.grid(True)																	  # Grid
 	plt.title(f'Distribution of all replies \n(abs.: {all_replies_sum}; avg.: {int(all_replies_sum / time_space)} per s)')
 		
-	plt.suptitle(f'Evaluation for the time from {str_time_begin[0:19]} to {str_time_end[0:19]} \nOccupancy of the channel: {round(occupancy_channel_sum, 3)}s \nFlights on average: {curr_planes}', fontsize=14)
+	plt.suptitle(f'Evaluation for the time from {str_time_begin[0:19]} to {str_time_end[0:19]} \nOccupancy of the channel: {round(occupancy_channel, 2)}s \nFlights on average: {curr_planes}', fontsize=14)
 						
 	plt.subplots_adjust(left = 0.07, bottom = 0.05, right = 0.95, top = 0.8, wspace = 0.25, hspace = 0.55)  # Distances of the sobplots
 	
